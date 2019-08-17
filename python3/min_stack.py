94490dbbe52dbc853cea867ffa83ae7055ed5401
davidchang <davidchang@ovotv.com>
2019-08-17 17:24:00 +0000

    :racehorse: faster version

--- python3/min_stack.py
+++ python3/min_stack.py
@@ -2,18 +2,27 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
+        self.minStack = []
 
     def push(self, x):
         self.stack.append(x)
+        try:
+            self.minStack.append(min(x, self.minTop()))
+        except:
+            self.minStack.append(x)
 
     def pop(self):
+        self.minStack.pop()
         return self.stack.pop()
 
     def top(self):
         return self.stack[-1]
 
+    def minTop(self):
+        return self.minStack[-1]
+
     def getMin(self):
-        return min(self.stack)
+        return self.minStack[-1]
 
 min_stack = MinStack()
 min_stack.push(2)
@@ -24,3 +33,5 @@ min_stack.push(-2)
 print(min_stack.pop())
 print(min_stack.top())
 print(min_stack.getMin())
+print(min_stack.minStack)
+print(min_stack.stack)
