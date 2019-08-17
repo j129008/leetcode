13c7d3bfbb1a292108a5ba06d545082108221485
davidchang <davidchang@ovotv.com>
2019-08-17 17:39:22 +0000

    :hammer: one stack version

--- python3/min_stack.py
+++ python3/min_stack.py
@@ -2,36 +2,24 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
-        self.minStack = []
 
     def push(self, x):
-        self.stack.append(x)
         try:
-            self.minStack.append(min(x, self.minTop()))
+            self.stack.append((x, min(x, self.getMin())))
         except:
-            self.minStack.append(x)
+            self.stack.append((x, x))
 
     def pop(self):
-        self.minStack.pop()
-        return self.stack.pop()
+        return self.stack.pop()[0]
 
     def top(self):
-        return self.stack[-1]
-
-    def minTop(self):
-        return self.minStack[-1]
+        return self.stack[-1][0]
 
     def getMin(self):
-        return self.minStack[-1]
+        return self.stack[-1][1]
 
 min_stack = MinStack()
-min_stack.push(2)
-min_stack.push(-1)
-min_stack.push(-3)
-min_stack.push(1)
 min_stack.push(-2)
-print(min_stack.pop())
-print(min_stack.top())
+min_stack.push(0)
+min_stack.push(1)
 print(min_stack.getMin())
-print(min_stack.minStack)
-print(min_stack.stack)
