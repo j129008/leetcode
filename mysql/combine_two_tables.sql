988fe199aef1a71f33229ed046edce35e81f5788
j129008 <j129008@gmail.com>
2021-01-12 18:46:31 +0000

    :bug: multiple line comment

--- mysql/combine_two_tables.sql
+++ mysql/combine_two_tables.sql
@@ -1,8 +1,8 @@
---
+/*
 https://leetcode.com/problems/combine-two-tables/submissions/
 
 left join 應用
---
+*/
 select FirstName, LastName, City, State from Person p
 left join Address a on
 p.PersonId = a.PersonId
--- mysql/customers_who_never_order.sql
+++ mysql/customers_who_never_order.sql
@@ -1,7 +1,7 @@
---
+/*
 1. subquery 的應用
 2. distinct 去除重複資料減少計算量
---
+*/
 select Name as Customers from Customers
 where Customers.Id not in (
     select distinct CustomerId from Orders
