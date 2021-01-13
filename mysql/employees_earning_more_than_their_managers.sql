2a7f6edf9d6050125d684ab5295382d14fba7997
j129008 <j129008@gmail.com>
2021-01-13 02:15:23 +0000

    :racehorse:

--- mysql/employees_earning_more_than_their_managers.sql
+++ mysql/employees_earning_more_than_their_managers.sql
@@ -6,4 +6,4 @@ https://leetcode.com/problems/employees-earning-more-than-their-managers
 
 select e.Name as Employee from Employee e
 join Employee m on m.id = e.ManagerId
-where e.Salary > m.Salary
+and e.Salary > m.Salary  # 這裏用 where 會慢很多, 在 join 的時候一起完成
